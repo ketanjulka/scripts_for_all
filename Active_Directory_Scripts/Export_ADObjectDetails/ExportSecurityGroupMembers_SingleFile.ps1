89d3514ce7d1728fb3ee18b83f99c336cb866136
Ketan Julka <ketan.julka@outlook.com>
2021-10-04 16:47:14 +0400

    Initial Commit

---
+++ Active_Directory_Scripts/Export_ADObjectDetails/ExportSecurityGroupMembers_SingleFile.ps1
@@ -0,0 +1,28 @@
+﻿# Import Active Directory Modules
+Import-Module ActiveDirectory
+
+$results = @()
+
+# Import group names.
+$inputgroup = Import-Csv -Path '\\auhfsv01\OpenShares\Ketan\newA1 - test.csv' -Encoding UTF8
+
+foreach($group in $inputgroup)
+{
+    $dlsa = $group.SamAccountName
+    $dltuse = $group.Name
+    ECHO "SamAccountName of DL in picture"$dlsa""
+    
+    Get-ADGroupMember -Identity $dlsa | ForEach-Object {
+    $member = $_
+    $results += New-Object -TypeName psobject -Property @{
+    GroupName = $dltuse
+    MemberName = $member.name
+    SamAccountName = $member.SamAccountName
+    objectClass = $member.objectClass
+    objectGUID = $member.objectGUID
+    SID = $member.SID
+    distinguishedName = $member.distinguishedName
+    }}
+        
+}
+$results | Select-Object GroupName,MemberName,SamAccountName,objectClass,objectGUID,distinguishedName,SID | Export-Csv "C:\Scripts\AD\AllGroupsMembers.csv" -NotypeInformation -Encoding UTF8
