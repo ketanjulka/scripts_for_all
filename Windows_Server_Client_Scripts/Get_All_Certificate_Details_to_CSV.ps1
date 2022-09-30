32a43e8cd4bf35702a150649b53a3c16b86bea8c
Ketan Julka <ketan.julka@outlook.com>
2022-09-30 17:42:33 +0400

    Added new script

---
+++ Windows_Server_Client_Scripts/Get_All_Certificate_Details_to_CSV.ps1
@@ -0,0 +1,1 @@
+certutil -view -out "RequestID,RequesterName,RequestType,NotAfter,CommonName,Certificate Template,Public Key Length,Certificate Effective Date,Certificate Expiration Date" csv > c:\temp\certs.csv
